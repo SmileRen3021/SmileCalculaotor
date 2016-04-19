7060776918207f780a533b219646b2170ac43e72
SmileRen3021 <irenxiaoyan@126.com>
2016-04-19 17:10:24 +0800

    B9 .c&&.h&&.exe

---
+++ smilelib.h
@@ -0,0 +1,69 @@
+// smilelib.h 
+// Smile's C Headerfile
+// Created by ²»¿¿Æ×xy on 16/4/14 
+// Last change on 16/4/15
+// Copyleft 2016 SmileRen3021. All lefts reserved.
+
+//use #include "smilelib.h"
+
+/*<begin>including #headerfiles#*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <windows.h>
+#include <time.h>
+#include <string.h> 
+
+/*<end>including #headerfiles#*/
+
+/*<begin>units converting*/
+
+//length
+
+#define mipkm 0.6214
+#define kmpmi 1.6093
+
+#define ftpm 3.2808
+#define mpft 0.3048
+
+#define inpcm 0.3937
+#define cmpin 2.54
+
+//area
+
+#define sqmipsqkm 0.3861
+#define sqkmpsqmi 2.5899
+
+#define sqftpsqm 10.7639
+#define sqmpsqft 0.0929
+
+#define sqinpsqcm 0.1550
+#define sqcmpsqin 6.4516
+
+//volume
+
+#define usptpl 2.1133
+#define lpuspt 0.4731
+
+#define cuydpcum 1.3079
+#define cumpcuyd 0.7645
+
+//weight
+
+#define lbpkg 2.2046
+#define kgplb 0.4535
+
+#define ozpg 0.0352
+#define gpoz 28.3495
+
+//temperature
+
+#define fpc 33.8
+#define cpf 0.38
+
+/*<end>units converting */
+
+/*<begin>*/
+
+/*<end>*/
